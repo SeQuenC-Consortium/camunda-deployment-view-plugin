c16778db20d78c43a1ddf8a776901acc0b7e0afd
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2023-10-27 17:37:10 +0100

    Add Labels to Nodes and Edges (#8)

--- frontend/src/OpenTOSCARenderer.js
+++ frontend/src/OpenTOSCARenderer.js
@@ -338,144 +338,286 @@ export default class OpenTOSCARenderer {
         }
     }
 
-    drawNodeConnections(parentGfx, topNode, relationshipTemplates, nodePositions) {
+    drawNodeConnections(
+        parentGfx,
+        topNode,
+        relationshipTemplates,
+        nodePositions
+      ) {
         const connectionCountAtNodeLocation = new Map();
         const connections = [];
-
+    
         const addToPort = (nodeRef, location) => {
-            const key = nodeRef + "-" + location;
-            let position;
-            if (connectionCountAtNodeLocation.has(key)) {
-                position = connectionCountAtNodeLocation.get(key) + 1;
-            } else {
-                position = 1;
-            }
-            connectionCountAtNodeLocation.set(key, position);
-            return position;
+          const key = nodeRef + "-" + location;
+          let position;
+          if (connectionCountAtNodeLocation.has(key)) {
+            position = connectionCountAtNodeLocation.get(key) + 1;
+          } else {
+            position = 1;
+          }
+          connectionCountAtNodeLocation.set(key, position);
+          return position;
         };
-
-        const addConnection = (sourceNode, sourceLocation, target, targetLocation) => {
-            connections.push({
-                source: sourceNode,
-                target,
-                sourceLocation,
-                targetLocation,
-                sourcePortIndex: addToPort(sourceNode.ref, sourceLocation),
-                targetPortIndex: addToPort(target.ref, targetLocation),
-            });
+    
+        const addConnection = (
+          sourceNode,
+          sourceLocation,
+          target,
+          targetLocation,
+          connectionName
+        ) => {
+          connections.push({
+            source: sourceNode,
+            target,
+            sourceLocation,
+            targetLocation,
+            sourcePortIndex: addToPort(sourceNode.ref, sourceLocation),
+            targetPortIndex: addToPort(target.ref, targetLocation),
+            connectionName,
+          });
         };
-
+    
         for (let relationshipTemplate of relationshipTemplates) {
-            const sourceRef = relationshipTemplate.sourceElement.ref;
-            const targetRef = relationshipTemplate.targetElement.ref;
-
-            const source = {
-                width: NODE_WIDTH,
-                height: sourceRef === topNode.id ? 80 : NODE_HEIGHT,
-                ref: sourceRef,
-                ...nodePositions.get(sourceRef)
-            };
-            const target = {
-                width: NODE_WIDTH,
-                height: sourceRef === topNode.id ? 80 : NODE_HEIGHT,
-                ref: targetRef,
-                ...nodePositions.get(targetRef)
-            };
-            const orientation = getOrientation(source, target, 0);
-
-            switch (orientation) {
-                case 'intersect':
-                case 'bottom':
-                    addConnection(source, 'north', target, 'south');
-                    break;
-                case 'top':
-                    addConnection(source, 'south', target, 'north');
-                    break;
-                case 'right':
-                    addConnection(source, 'east', target, 'west');
-                    break;
-                case 'left':
-                    addConnection(source, 'west', target, 'east');
-                    break;
-                case 'top-left':
-                    addConnection(source, 'south', target, 'east');
-                    break;
-                case 'top-right':
-                    addConnection(source, 'south', target, 'west');
-                    break;
-                case 'bottom-left':
-                    addConnection(source, 'north', target, 'east');
-                    break;
-                case 'bottom-right':
-                    addConnection(source, 'north', target, 'west');
-                    break;
-                default:
-                    return;
-            }
+          const sourceRef = relationshipTemplate.sourceElement.ref;
+          const targetRef = relationshipTemplate.targetElement.ref;
+          const source = {
+            width: NODE_WIDTH,
+            height: sourceRef === topNode.id ? 80 : NODE_HEIGHT,
+            ref: sourceRef,
+            ...nodePositions.get(sourceRef),
+          };
+          const target = {
+            width: NODE_WIDTH,
+            height: sourceRef === topNode.id ? 80 : NODE_HEIGHT,
+            ref: targetRef,
+            ...nodePositions.get(targetRef),
+          };
+          const orientation = getOrientation(source, target, 0);
+    
+          switch (orientation) {
+            case "intersect":
+            case "bottom":
+              addConnection(
+                source,
+                "north",
+                target,
+                "south",
+                relationshipTemplate.name
+              );
+              break;
+            case "top":
+              addConnection(
+                source,
+                "south",
+                target,
+                "north",
+                relationshipTemplate.name
+              );
+              break;
+            case "right":
+              addConnection(
+                source,
+                "east",
+                target,
+                "west",
+                relationshipTemplate.name
+              );
+              break;
+            case "left":
+              addConnection(
+                source,
+                "west",
+                target,
+                "east",
+                relationshipTemplate.name
+              );
+              break;
+            case "top-left":
+              addConnection(
+                source,
+                "south",
+                target,
+                "east",
+                relationshipTemplate.name
+              );
+              break;
+            case "top-right":
+              addConnection(
+                source,
+                "south",
+                target,
+                "west",
+                relationshipTemplate.name
+              );
+              break;
+            case "bottom-left":
+              addConnection(
+                source,
+                "north",
+                target,
+                "east",
+                relationshipTemplate.name
+              );
+              break;
+            case "bottom-right":
+              addConnection(
+                source,
+                "north",
+                target,
+                "west",
+                relationshipTemplate.name
+              );
+              break;
+            default:
+              return;
+          }
         }
-
+    
         for (const connection of connections) {
-            const getPortPoint = (element, location, locationIndex) => {
-                const portCount = connectionCountAtNodeLocation.get(element.ref + "-" + location);
-                if (location === 'north') {
-                    return {
-                        x: element.x + (element.width / (portCount + 1)) * locationIndex,
-                        y: element.y
-                    };
-                } else if (location === 'south') {
-                    return {
-                        x: element.x + (element.width / (portCount + 1)) * locationIndex,
-                        y: element.y + element.height
-                    };
-                } else if (location === 'east') {
-                    return {
-                        x: element.x,
-                        y: element.y + (element.height / (portCount + 1)) * locationIndex
-                    };
-                } else if (location === 'west') {
-                    return {
-                        x: element.x + element.width,
-                        y: element.y + (element.height / (portCount + 1)) * locationIndex
-                    };
-                }
-            };
-
-            const getSimpleDirection = direction => direction === 'north' || direction === 'south' ? 'v' : 'h';
-
-            const points = connectPoints(
-                getPortPoint(connection.source, connection.sourceLocation, connection.sourcePortIndex),
-                getPortPoint(connection.target, connection.targetLocation, connection.targetPortIndex),
-                getSimpleDirection(connection.sourceLocation) + ":" + getSimpleDirection(connection.targetLocation)
+          const getPortPoint = (element, location, locationIndex) => {
+            const portCount = connectionCountAtNodeLocation.get(
+              element.ref + "-" + location
             );
-
-            const line = createLine(points, this.styles.computeStyle({}, ['no-fill'], {
-                ...STROKE_STYLE,
-                markerEnd: `url(#${DEPLOYMENT_REL_MARKER_ID})`
-            }), 5);
-            parentGfx.prepend(line);
+            if (location === "north") {
+              return {
+                x: element.x + (element.width / (portCount + 1)) * locationIndex,
+                y: element.y,
+              };
+            } else if (location === "south") {
+              return {
+                x: element.x + (element.width / (portCount + 1)) * locationIndex,
+                y: element.y + element.height,
+              };
+            } else if (location === "east") {
+              return {
+                x: element.x,
+                y: element.y + (element.height / (portCount + 1)) * locationIndex,
+              };
+            } else if (location === "west") {
+              return {
+                x: element.x + element.width,
+                y: element.y + (element.height / (portCount + 1)) * locationIndex,
+              };
+            }
+          };
+    
+          const getSimpleDirection = (direction) =>
+            direction === "north" || direction === "south" ? "v" : "h";
+    
+          const points = connectPoints(
+            getPortPoint(
+              connection.source,
+              connection.sourceLocation,
+              connection.sourcePortIndex
+            ),
+            getPortPoint(
+              connection.target,
+              connection.targetLocation,
+              connection.targetPortIndex
+            ),
+            getSimpleDirection(connection.sourceLocation) +
+              ":" +
+              getSimpleDirection(connection.targetLocation)
+          );
+    
+          const line = createLine(
+            points,
+            this.styles.computeStyle({}, ["no-fill"], {
+              ...STROKE_STYLE,
+              markerEnd: `url(#${DEPLOYMENT_REL_MARKER_ID})`,
+            }),
+            5
+          );
+    
+          const labelGroup = svgCreate("g");
+    
+          const pathLength = line.getTotalLength();
+          const middlePoint = line.getPointAtLength(pathLength / 2);
+          svgAttr(labelGroup, {
+            transform: `matrix(1, 0, 0, 1, ${(
+              middlePoint.x -
+              LABEL_WIDTH / 2
+            ).toFixed(2)}, ${(middlePoint.y - LABEL_HEIGHT / 2).toFixed(2)})`,
+          });
+          const backgroundRect = svgCreate("rect", {
+            width: LABEL_WIDTH,
+            height: LABEL_HEIGHT,
+            fill: "#EEEEEE",
+            fillOpacity: 0.75,
+          });
+          svgAppend(labelGroup, backgroundRect);
+          const text = this.textRenderer.createText(connection.connectionName, {
+            box: {
+              width: LABEL_WIDTH,
+              height: LABEL_HEIGHT,
+            },
+            align: "center-middle",
+            style: {
+              fontSize: 10,
+            },
+          });
+          svgAppend(labelGroup, text);
+          parentGfx.prepend(labelGroup);
+          parentGfx.prepend(line);
         }
-    }
+      }
 
     drawNodeTemplate(parentGfx, nodeTemplate, position) {
-        const groupDef = svgCreate('g');
-        svgAttr(groupDef, {transform: `matrix(1, 0, 0, 1, ${position.x.toFixed(2)}, ${position.y.toFixed(2)})`});
-        const rect = svgCreate('rect', {
-            width: NODE_WIDTH,
-            height: NODE_HEIGHT,
-            fill: '#DDDDDD',
-            ...STROKE_STYLE
+        const groupDef = svgCreate("g");
+        svgAttr(groupDef, {
+          transform: `matrix(1, 0, 0, 1, ${position.x.toFixed(
+            2
+          )}, ${position.y.toFixed(2)})`,
         });
-
+        const rect = svgCreate("rect", {
+          width: NODE_WIDTH,
+          height: NODE_HEIGHT,
+          fill: "#DDDDDD",
+          ...STROKE_STYLE,
+        });
+    
         svgAppend(groupDef, rect);
-
+    
         const text = this.textRenderer.createText(nodeTemplate.name, {
-            box: {
-                width: NODE_WIDTH,
-                height: NODE_HEIGHT,
-            },
-            align: 'center-middle'
+          box: {
+            width: NODE_WIDTH,
+            height: NODE_HEIGHT / 2,
+          },
+          align: "center-middle",
         });
+    
         svgAppend(groupDef, text);
+    
+        const groupDef2 = svgCreate("g");
+        svgAttr(groupDef2, {
+          transform: `matrix(1, 0, 0, 1, ${position.x.toFixed(2)}, ${(
+            position.y +
+            NODE_HEIGHT / 2
+          ).toFixed(2)})`,
+        });
+    
+        const namePattern = /\}(.*)/g;
+        var typeMatches = namePattern.exec(nodeTemplate.type);
+        var typeName;
+        if (typeMatches === null && typeMatches.length < 1) {
+          typeName = nodeTemplate.type;
+        } else {
+          typeName = typeMatches[1];
+        }
+    
+        const typeText = this.textRenderer.createText("(" + typeName + ")", {
+          box: {
+            width: NODE_WIDTH,
+            height: NODE_HEIGHT / 2,
+          },
+          align: "center-middle",
+          style: {
+            fill: "#777777",
+          },
+        });
+    
+        svgAppend(groupDef2, typeText);
         parentGfx.append(groupDef);
-    }
+        parentGfx.append(groupDef2);
+      }
 }
